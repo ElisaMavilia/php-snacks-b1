b78c8053604cbe1989f55902f2b698f3d4413d64
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-30 18:06:31 +0200

    adjustments

--- snack2.php
+++ snack2.php
@@ -5,7 +5,20 @@ Con un form passare come parametri GET name, mail e age e verificare (cercando i
 
 <?php 
 $userdata = $_GET;
-var_dump($userdata)
+
+var_dump($userdata);
+
+ if (empty($_GET['username'])) {
+    echo "Insert your name";
+ } else {
+    $username = $_GET['username'];
+ };
+
+ if (strlen($_GET['username']) < 3){
+    echo 'Min 3 characters required';
+} else{
+    $username = $_GET['username'];
+}
 ?>
 
  <!DOCTYPE html>
@@ -20,11 +33,29 @@ var_dump($userdata)
 <body>
     <main >
         <form action="snack2.php" method="GET" name="userdata">
-            <input type="text" name="name">
+            <input type="text" name="username">
             <input type="email" name="email">
-            <input type="number" name="">
-            <button type="submit" class="btn btn-success">Send</button>
+            <input type="number" name="number">
+            <button type="submit" name="submit" class="btn btn-success">Send</button>
         </form>
+        <div>
+           <?php
+            if (empty($_GET['username'])) {
+                echo "Insert your name";
+             } else {
+                $username = $_GET['username'];
+             };
+           ?>
+        </div>
+        <div>
+            <?php  
+             if (strlen($_GET['username']) < 3){
+                echo 'Min 3 characters required';
+            } else{
+                $username = $_GET['username'];
+            }
+            ?>
+        </div>
     </main>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
